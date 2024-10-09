e66b8a8e11b74ebba6f9fd3f9424526f06eb8215
Mr-Ndi <onlythenotes@gmail.com>
2024-10-09 17:29:07 +0200

    after merging

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,7 @@ dotenv.config();
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser:true,
-            useUnifiedTopology: true
+            useNewUrlParser:true
         });
         console.log("DB connection successful!");
     } catch (error) {
