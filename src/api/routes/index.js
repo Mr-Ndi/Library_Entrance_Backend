8b3111393e6fa1f4a9b8b96b564701d9baacc6a6
Mr-Ndi <onlythenotes@gmail.com>
2024-10-24 12:28:06 +0200

    merging the codes

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -5,7 +5,18 @@ import schoolRoutes from "./school.routes.js";
 
 const routes = Router();
 
-routes.use('/school')
+routes.use('/school', schoolRoutes)
+routes.use('/history', historyRoutes);
+routes.use('/user', userRoutes);
+
+export default routes;
+import { Router } from "express";
+import historyRoutes from "./history.routes.js";
+import userRoutes from "./user.routes.js";
+
+const routes = Router();
+
+// routes.use('/school')
 routes.use('/history', historyRoutes);
 routes.use('/user', userRoutes);
 
